a0cb21ae7246cc1a6ecf6c61cf08678ef1cf1323
Phengar <59420517+Phengar@users.noreply.github.com>
2022-03-28 14:38:11 +0200

    implement the format of data for graphs

--- format.h
+++ format.h
@@ -1,6 +1,12 @@
-typedef struct{
+typedef struct _UP{
     unsigned char name;
     int semestre;
     int id;
+    struct _UP * next;
 }UP;
 
+typedef struct{
+    int nb_n; //nb of nodes
+    int nb_e; //nb of edges
+    UP ** g;
+}graph;
