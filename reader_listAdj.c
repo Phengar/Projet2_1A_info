aef9c3dc1e8e9baa28d3da0d30b7305c9374eee8
Phengar <59420517+Phengar@users.noreply.github.com>
2022-04-04 17:16:38 +0200

    modify the read function

--- reader_listAdj.c
+++ reader_listAdj.c
@@ -1,7 +1,28 @@
 /////////////////////////////////////////////////////////////////////////////////
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "format.h"
 #include "reader_listAdj.h"
 
 /////////////////////////////////////////////////////////////////////////////////
+
+graph read(){
+    char nom[50];
+    int x,y;
+    FILE * file;
+    file = fopen("nodes.csv", "rb");
+    if (file == NULL){
+        fprintf(stderr, "\nCouldn't  Open nodes.csv\n");
+        exit(1);
+    }
+    fscanf(file, "%s,%d,%d", nom, &x, &y);
+    while (fscanf(file, "%s,%d,%d", nom, &x, &y)== 3){
+
+    }
+}
+
+int ** main(){
+
+}
 
 /////////////////////////////////////////////////////////////////////////////////
