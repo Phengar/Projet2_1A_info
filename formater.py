1ccb7f6577fe3c24c324328047afb2daf03f82bd
Phengar <59420517+Phengar@users.noreply.github.com>
2022-04-07 01:06:04 +0200

    Implement a clean way to modify files from .bak

--- formater.py
+++ formater.py
@@ -13,8 +13,10 @@
 
 #################################################################################
 
-NODES_CSV = "nodes.csv"
-EDGES_CSV = "edges.csv"
+NODES_CSV_IN = "nodes.csv.bak"
+NODES_CSV_OUT = "nodes.csv"
+EDGES_CSV_IN = "edges.csv.bak"
+EDGES_CSV_OUT = "edges.csv"
 
 dump = [] # Dump list that contains the file content processed
 hashtable = {} # Hashtable to avoid duplicates
@@ -24,40 +26,40 @@ hashtable = {} # Hashtable to avoid duplicates
 # ------------------------------------------------------------------------------#
 # Formating the edges file :												 	#
 # ------------------------------------------------------------------------------#
-#file_in = open(EDGES_CSV, "r")
-#file_in.readline() # Avoiding first row
-#
-#for line in file_in:
-#	if(not line in hashtable): # Checks for any duplicates
-#		hashtable[line] = 1
-#		string = line.replace(" ", "") # Deletes all blank characters
-#		L = string.split(",")
-#		# Checks for loop edges
-#		if((line[1:] != L[2][1:]) or (L[1][:-1] != L[3][:-2])):
-#			string = ",".join(L)
-#			string = string.replace("[", "")
-#			string = string.replace("]", "")
-#			string = string.replace(",", " ")
-#			dump.append(string)
-#
-#file_in.close()
-#
-## ------------------------------------------------------------------------------#
-## Saving the formated edges file	:										    #
-## ------------------------------------------------------------------------------#
-#file_out = open(EDGES_CSV, "w")
-#
-#for l in dump:
-#	file_out.write(l)
-#
-#file_out.close()
+file_in = open(EDGES_CSV_IN, "r")
+file_in.readline() # Avoiding first row
+
+for line in file_in:
+	if(not line in hashtable): # Checks for any duplicates
+		hashtable[line] = 1
+		string = line.replace(" ", "") # Deletes all blank characters
+		L = string.split(",")
+		# Checks for loop edges
+		if((line[1:] != L[2][1:]) or (L[1][:-1] != L[3][:-2])):
+			string = ",".join(L)
+			string = string.replace("[", "")
+			string = string.replace("]", "")
+			string = string.replace(",", " ")
+			dump.append(string)
+
+file_in.close()
+
+# ------------------------------------------------------------------------------#
+# Saving the formated edges file	:										    #
+# ------------------------------------------------------------------------------#
+file_out = open(EDGES_CSV_OUT, "w")
+
+for l in dump:
+	file_out.write(l)
+
+file_out.close()
 
 #################################################################################
 
 # ------------------------------------------------------------------------------#
 # Formating the nodes file :									                #	
 # ------------------------------------------------------------------------------#
-file_in = open(NODES_CSV, "r")
+file_in = open(NODES_CSV_IN, "r")
 file_in.readline() # Avoiding first row
 dump = []
 i = 0
@@ -76,7 +78,7 @@ file_in.close()
 # ------------------------------------------------------------------------------#
 # Saving the formated nodes file	:										    #
 # ------------------------------------------------------------------------------#
-file_out = open(NODES_CSV, "w")
+file_out = open(NODES_CSV_OUT, "w")
 
 for l in dump:
 	file_out.write(l)
