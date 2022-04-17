776e78c07bd8b25ce4d509a03b345b7f103a001b
Banji <38909289+TheBanji@users.noreply.github.com>
2022-04-17 18:58:36 +0200

    Adjust in the result layout
    Adjust in the layout printed by the program

--- main.c
+++ main.c
@@ -28,13 +28,14 @@ int main() {
     mat_adj * graph =  NULL;
     load_graph("./csv/nodes.csv", "./csv/edges.csv", &graph);
     int nodes = get_nodes(graph);
+    printf("================================\n\n");
 	printf("Number of nodes in the graph : %d\n", nodes);
-    printf("================================\n");
+    printf("\n================================\n\n");
 
 
     printf("Number of UP in the ISMIN syllabus : %d.\n", get_UP(graph));
     printf("Number of GP in the ISMIN syllabus : %d.\n", get_GP(graph));
-    printf("================================\n");
+    printf("\n================================\n\n");
 
 
     int semester = 5, year = 1;
@@ -42,7 +43,7 @@ int main() {
     printf("Number of UP in the year %d : %d.\n", year, get_UP_year(graph, year));
     printf("Number of GP in the %d-th semester : %d.\n", semester, get_GP_semester(graph, semester));
     printf("Number of GP in the year %d : %d.\n", year, get_GP_year(graph, year));
-    printf("================================\n");
+    printf("\n================================\n\n");
 
 
     int id_max_degree, id_min_degree;
@@ -50,7 +51,7 @@ int main() {
     int max_degree = get_max_degree(graph, &id_max_degree);
     printf("Minimum degree node : %s - %d.\n", graph[id_min_degree].name, min_degree);
     printf("Maximum degree node : %s - %d.\n", graph[id_max_degree].name, max_degree);
-    printf("================================\n");
+    printf("\n================================\n\n");
 
 
     int * degrees = (int *) calloc(get_nodes(graph), sizeof(int));
@@ -61,11 +62,11 @@ int main() {
     get_degrees(graph, degrees);
     printf("Degrees per node :\n");
     print_array(degrees, get_nodes(graph));
-    printf("================================\n");
+    printf("\n================================\n\n");
 
 
     print_top_degrees(degrees, graph, 10);
-    printf("================================\n");
+    printf("\n================================\n\n");
 
 
     int length;
@@ -77,17 +78,17 @@ int main() {
     }
     get_degrees_GP(graph, gp_array, length, degrees_GP);
     print_top_degrees_GP(graph, gp_array, degrees_GP, length, 5);
-    printf("================================\n");
+    printf("\n================================\n\n");
 
     printf("Is the graph acyclic : %s\n", (is_acyclic(graph)) ? "oui" : "non");
-    printf("================================\n");
+    printf("\n================================\n\n");
 
 
     cluster * clus = NULL;
     cluster_search(graph, &clus);
     printf("Result of the cluster search in the graph :\n");
     print_cluster(graph, &clus);
-    printf("================================\n");
+    printf("\n================================\n\n");
 
 
     // Creates the graph de précédence from the syllabus graph
@@ -106,13 +107,13 @@ int main() {
     int mode = 0;
     int lpath = longest_path(precedence_graph, q, mode);
     printf("Longest path in the graph : %d.\n", lpath);
-    printf("================================\n");
+    printf("\n================================\n\n");
 
 
     queue * tmp = NULL;
     for(int u = 0; u < get_nodes(precedence_graph); u++) {
         tmp = q[u];
-        printf("%s(%d) -> ", precedence_graph[u].name, u);
+        printf("(%d) -> ", u);
         while(tmp != NULL) {
             printf("%s(%d), ", precedence_graph[tmp->key].name, tmp->key);
             tmp = tmp->next;
